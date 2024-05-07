750bd53a5f0b90ef4d0d3882ca5ddfead708e496
GrisGGG <astropoetscapicua@gmail.com>
2024-05-06 23:35:53 -0600

    Iniciando el módulo MVC

---
+++ aprendiendo_php_mvc/apuntes.php
@@ -0,0 +1,16 @@
+<?php
+
+//MVC 
+//Es un patrón de diseño de software que se encarga de separar la funcionalidad
+//de la vista ósea lo que se le muestra al usuario.
+//Modelo- Lógica del negocio, las clases o métodos que se comunican con la b.d
+//Vista- Se encarga de mostrar la información al usuario.
+//Controlador- Intermediario entre modelo y vista, si el usuario hace una petición
+//el controlador procesa esa info y se la pide al modelo y devuelve una vista nueva.
+//Funcionamiento Básico del MVC
+//1-El usuario realiza una petición.
+//2-El controlador captura la petición.
+//3-Hace la llamada al modelo correspondiente.
+//4-El modelo sera el encargado de interactuar con la b.d
+//5- El controlador recibe la info y la envia a la vista.
+//6- La vista muestra la info.
